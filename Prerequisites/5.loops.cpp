073d258b030ce8eb10f99b9ee1b00ad5555b45e9
Kraken57 <kasaad1609@gmail.com>
2023-12-18 18:07:22 +0530

    Loops :|

---
+++ Prerequisites/5.loops.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    freopen("coutin.txt", "r",stdin);
+    freopen("countout.txt","w",stdout);
+
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    
+//can be done by for loop too
+    return 0;
+}
